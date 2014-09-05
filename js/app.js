8f1e8abc56cb44290e922edbab52d2835ca75b3f
Pavel Kovalyov <sxe.funky@gmail.com>
2014-09-05 23:46:11 +0300

    Gallery

--- js/app.js
+++ js/app.js
@@ -11,14 +11,34 @@
 			price: 2.95,
 			description: ' . . . ',
 			canPurchase: false,
-			soldOut: true
+			soldOut: true,
+			images: [
+				{
+					full: 'http://lorempixel.com/400/200/food/1',
+					thumb: 'http://lorempixel.com/50/50/food/1'
+				},
+				{
+					full: 'http://lorempixel.com/400/200/food/2',
+					thumb: 'http://lorempixel.com/50/50/food/2'
+				},
+			]
 		},
 		{
 			name: 'Pentagonal Gem',
 			price: 4.95,
 			description: ' . . . ',
 			canPurchase: false,
-			soldOut: true
+			soldOut: true,
+			images: [
+				{
+					full: 'http://lorempixel.com/400/200/food/3',
+					thumb: 'http://lorempixel.com/50/50/food/3'
+				},
+				{
+					full: 'http://lorempixel.com/400/200/food/4',
+					thumb: 'http://lorempixel.com/50/50/food/4'
+				}
+			]
 		}
 	]
 })();
