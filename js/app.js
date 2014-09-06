78843a52347c39702143c97f8d01064279cef2f2
Pavel Kovalyov <sxe.funky@gmail.com>
2014-09-06 18:05:34 +0300

    Review Controller

--- js/app.js
+++ js/app.js
@@ -15,7 +15,16 @@
 		this.isSelected = function (checkTab) {
 			return this.tab === checkTab;
 		}
-	})
+	});
+
+	app.controller('ReviewController', function () {
+		this.review = {};
+
+		this.addReview = function (product) {
+			product.reviews.push(this.review);
+			this.review = {};
+		};
+	});
 
 	var gems = [
 		{
